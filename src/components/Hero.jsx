e3239194a4e6fef73050fd1840029c028e41fc0a
dashing-arpit <yoarpits30@gmail.com>
2023-05-31 01:23:22 +0000

    second push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/dashing-arpit/Ai-summerizer/tree/master", "_blank")
           }
           className='black_btn'
         >
